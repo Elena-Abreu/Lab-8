f638baf7188af84859565375c68bb88e036c653e
Elena-Abreu <ena3@njit.edu>
2024-11-14 10:27:43 -0500

    Question 2

--- scriptOne.js
+++ scriptOne.js
@@ -3,6 +3,15 @@ var firstname = "Elena";
 console.log(firstname);
 
 var age; 
-var age = "18";
+var age = 18;
 console.log(age);
 
+
+let pet = false;
+console.log(pet) ;
+
+let house; 
+console.log(house)
+
+let bignumber = 56789876543456789876545678n ; 
+console.log(bignumber)
