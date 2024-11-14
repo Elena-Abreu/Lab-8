6c30174d16dd173505751ed96af996ee6ebed564
Elena-Abreu <ena3@njit.edu>
2024-11-14 10:31:29 -0500

    Question 3

--- scriptOne.js
+++ scriptOne.js
@@ -14,4 +14,16 @@ let house;
 console.log(house)
 
 let bignumber = 56789876543456789876545678n ; 
-console.log(bignumber)
+console.log(bignumber) 
+
+ 
+let answer;
+let RollNumber = 15; 
+
+
+if (RollNumber > 10) 
+{ 
+    answer = "The RollNumber is greater than 10"; 
+    console.log(answer);
+} 
+
